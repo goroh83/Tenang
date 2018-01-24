6ece8d74656f81ac545c0b36db66c22a67e0814e
Peter <goroh83@yahoo.com>
2018-01-24 12:46:14 -0500

    add navigation js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-const navigation = document.getElementById('navigation');
-const burger = document.getElementById('burger');
+const burger = document.querySelector('.nav__menu');
+const navigation = document.querySelector('nav');
+const navigationList = document.querySelector('.navigation');
+const closeX = document.querySelector('.nav__close');
+const toggle = document.querySelector('.toggle');
+const burger1 = document.querySelector('.bar1');
+const burger2 = document.querySelector('.bar2');
+const burger3 = document.querySelector('.bar3');
+const burger4 = document.querySelector('.bar4');
 
 
-burger.addEventListener('click', () =>{
-    console.log('works');
-    navigation.classList.toggle('nav-hide');
+toggle.addEventListener('click', () => {
+    burger1.classList.toggle('bar1active');
+    burger2.classList.toggle('bar2active');
+    burger3.classList.toggle('bar3active');
+    burger4.classList.toggle('bar4active');
+    navigation.classList.toggle('unhide');  
+    navigationList.classList.toggle('nav-unhide');
 });
